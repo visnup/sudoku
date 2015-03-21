3ac6c0268b2e99811419bf803a51015b43298370
Visnu Pitiyanuvath <visnupx@gmail.com>
2015-03-21 14:35:58 -0700

    faster by picking next cell strategically

--- index.js
+++ index.js
@@ -10,34 +10,41 @@ var hard = [ '6  2   3 ',
              '4   3  2 ',
              ' 7   9  5' ];
 var board = hard.map(function(s) { return s.split('').map(Number) });
+var numbers = _.shuffle(_.range(1, 10));
+
+console.log((function solve(board, cell) {
+  if (!cell) return board;
+
+  var r = cell[0], c = cell[1], guesses = cell[2], nextBoard;
+  if (guesses.length) {
+    for (var i = 0; i < guesses.length; i++) {
+      board[r][c] = guesses[i];
+      if (nextBoard = solve(board, next(board)))
+        return nextBoard;
+    }
+    board[r][c] = 0;  // back out
+  }
+})(board, next(board)));
 
 function taken(board, r, c) {
-  var br = Math.floor(r/3)*3,
-      bc = Math.floor(c/3)*3;
+  var br = Math.floor(r/3)*3, bc = Math.floor(c/3)*3;
   var row = board[r],
       col = _.pluck(board, c),
       box = board.slice(br, br+3).map(function(r) { return r.slice(bc, bc+3) });
   return row.concat(col, box[0], box[1], box[2]);
 }
 
-var numbers = _.shuffle(_.range(1, 10));
-console.log((function solve(board, cell) {
-  if (cell === 81)
-    return board;
-
-  var r = Math.floor(cell / 9),
-      c = cell % 9;
-
-  if (board[r][c])
-    return solve(board, cell+1);
-
-  var guesses = _.difference(numbers, taken(board, r, c)), nextBoard;
-  if (guesses.length) {
-    for (var i = 0; i < guesses.length; i++) {
-      board[r][c] = guesses[i];
-      if (nextBoard = solve(board, cell+1))
-        return nextBoard;
-    }
-    board[r][c] = 0;  // back out
-  }
-})(board, 0));
+function next(board) {
+  var cell, least = 10;
+  _.forEach(board, function(row, r) {
+    _.forEach(row, function(v, c) {
+      if (v) return;
+      var guesses = _.difference(numbers, taken(board, r, c));
+      if (guesses.length < least) {
+        cell = [r, c, guesses];
+        least = guesses.length;
+      }
+    });
+  });
+  return cell;
+}
